221010a80dee8ec210d58e5621ec3ec737130a0b
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2023-05-01 18:25:58 -0400

    Added Minecraft Manipulator Branding

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Legacy-Docs
 
 Documentation about Minecraft: Legacy Console Edition with Vue Press
+
+## To Build
+
+```bash
+yarn install
+yarn docs:build
+```
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ import { searchPlugin } from '@vuepress/plugin-search'
 import sidebar from './sidebar';
 
 let title = 'Minecraft Legacy Docs';
-let description = 'Just playing around';
+let description = 'Documentation about Minecraft: Legacy Console Edition';
 
 export default {
     base: '/Legacy-Docs/',
@@ -13,26 +13,26 @@ export default {
     head: [
         ['meta', { charset: 'utf-8' }],
         ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
-        // ['link', { rel: 'icon', href: '/favicon.png' }],
-        ['meta', { name: 'theme-color', content: '#27ae60' }],
+        ['link', { rel: 'icon', href: '/favicon.png' }],
+        ['meta', { name: 'theme-color', content: '#fa2426' }],
         ['meta', { name: 'twitter:card', content: 'summary' }],
         ['meta', { property: 'og:title', content: title }],
         ['meta', { property: 'og:description', content: description }],
         ['meta', { property: 'og:type', content: 'website' }],
-        // ['meta', { property: 'og:url', content: 'https://example.com/' }],
+        ['meta', { property: 'og:url', content: 'https://minecraft-manipulator.github.io/Legacy-Docs/' }],
         ['meta', { property: 'og:locale', content: 'en_US' }],
-        // ['meta', { property: 'og:image', content: 'https://example.com/' }],
+        ['meta', { property: 'og:image', content: 'https://minecraft-manipulator.github.io/Legacy-Docs/' }],
     ],
     theme: defaultTheme({
         sidebar,
         contributors: true,
-        repo: 'ModifiedCommand/Legacy-Docs',
-        //logo: '/favicon.png',
+        repo: 'Minecraft-Manipulator/Legacy-Docs',
+        logo: '/favicon.png',
         sidebarDepth: 2,
         docsDir: 'docs',
         editLink: true,
         lastUpdated: true,
-        colorMode: 'dark'
+        colorMode: 'auto'
     }),
     plugins: [
         searchPlugin({
