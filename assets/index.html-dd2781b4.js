a95a1a313bc50620e9ac78cdce19cc0805044cee
EternalModz <86510009+EternalModz@users.noreply.github.com>
2024-03-01 21:56:40 +0000

    Deploying to gh-pages from @ Team-Lodestone/Legacy-Docs@b956c0150e7342cfee748d677eaca23094754158 🚀

---
+++ assets/index.html-dd2781b4.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-12759b28","path":"/Formats/","title":"Home","lang":"en-US","frontmatter":{"title":"Home"},"headers":[],"git":{"updatedTime":1709330175000,"contributors":[{"name":"EternalModz","email":"86510009+EternalModz@users.noreply.github.com","commits":1}]},"filePathRelative":"Formats/README.md"}');export{t as data};
